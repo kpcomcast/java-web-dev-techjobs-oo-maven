3dece62bf680597ce043b40926dcfd412436c20b
Kiran Poulsen <kiran_poulsen@comcast.com>
2021-07-07 12:16:38 -0600

    assignment 2 first half

--- src/main/java/com/company/Job.java
+++ src/main/java/com/company/Job.java
@@ -15,4 +15,39 @@ public class Job {
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields match.
 
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID and id.
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public Employer getEmployer() {
+        return employer;
+    }
+    public Location getLocation() {
+        return location;
+    }
+    public PositionType getPositionType() {
+        return positionType;
+    }
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+    public void setCoreCompetency(CoreCompetency coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
 }
